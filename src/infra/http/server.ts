9f43465cd027cc28a2dea50c6380c8db6aa66946
albberrrt <albberrrt.yayay@gmail.com>
2024-02-08 14:11:34 -0300

    add createPoll route

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,26 +1,12 @@
 import { app } from "./app"
-import { prisma } from "../prisma/client"
-import { z } from "zod"
 import { env } from "../../config/env"
+import { createPoll } from "./routes/create-poll"
 
 app.get('/', () => {
   return "Hello World!"
 })
 
-app.post('/polls', async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string()
-  })
-  const { title } = createPollBody.parse(request.body)
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    }
-  })
-
-  return reply.status(201).send({ pollId: poll.id })
-})
+app.register(createPoll)
 
 app.listen({ host: env.SERVER_HOST, port: env.SERVER_PORT }).then(() => {
   console.log(`Server started! - ${env.SERVER_HOST}:${env.SERVER_PORT}  || http://${env.SERVER_HOST}:${env.SERVER_PORT}/`)
